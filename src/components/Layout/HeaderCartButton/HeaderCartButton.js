746f4f246a3a57678ebe4cbd5b7bd308219409e6
Gabriel Ballester <gaby.ballester@gmail.com>
2021-05-03 18:54:50 +0200

    Add Cart into App

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import CartIcon from '../../Cart/CartIcon';
+import CartIcon from '../../Cart/CartIcon.js';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
